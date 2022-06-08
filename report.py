7e19d9f87e8e5cf1fffba4bdb83d0bb02a7ec145
rjames187 <rory.james2021@gmail.com>
2022-06-07 20:55:42 -0500

    write and test sort_pages func

--- report.py
+++ report.py
@@ -3,4 +3,11 @@ def remove_none_values(dic : dict):
         if value == None:
             del dic[key]
     return dic
+
+def sort_pages(dic : dict):
+    def ret_val(tup : tuple):
+        return tup[1]
+    lst = list(dic.items())
+    lst.sort(reverse=True, key=ret_val)
+    return lst
     
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from report import remove_none_values
+from report import sort_pages
 
 
 class Tests(unittest.TestCase):
@@ -9,6 +10,11 @@ class Tests(unittest.TestCase):
         self.assertEqual({"1": "1"}, remove_none_values({"1": "1", "2": None}))
         self.assertEqual({}, remove_none_values({}))
         self.assertEqual({"1": "1"}, remove_none_values({"1": "1"}))
+    
+    def test_sort_pages(self):
+        self.assertEqual([("first", 45), ("second", 25), ("third", 15)], sort_pages({"first": 45, "third": 15, "second": 25}))
+        self.assertEqual([], sort_pages({}))
+        
 
 
 if __name__ == "__main__":
